fbc7bd8b4f11532878a99e80d0e3319771f2d75d
Katijino <powers2@rpi.edu>
2025-11-15 16:12:52 -0500

    added functionality for main.py to connect to tictactoe and return numplayers

--- server/main.py
+++ server/main.py
@@ -5,13 +5,28 @@ import globals
 IP_ADDRESS = "192.168.0.233"
 PORT = 8080
 
+def num_players(client_socket):
+    print(len(globals.players))
+    packet = {"type": "numplayers_answer", "num": len(globals.players)}
+    json_data = json.dumps(packet).encode("utf-8")
+    client_socket.sendall(json_data)
+
+def load_tic(client_socket):
+    packet = {"type": "tictactoe_confirm"}
+    json_data = json.dumps(packet).encode("utf-8")
+    client_socket.sendall(json_data)
+
+def load_black(client_socket):
+    packet = {"type": "blackjack_confirm"}
+    json_data = json.dumps(packet).encode("utf-8")
+    client_socket.sendall(json_data)
 
 def error_packet(client_socket):
     packet = {"type": "error"}
     json_data = json.dumps(packet).encode("utf-8")
     client_socket.sendall(json_data)
 
-def login_player(client_socket,login):
+def login_player(client_socket):
     if globals.players[0] != client_socket:
         pt = "player"
     else:
@@ -32,17 +47,22 @@ def on_new_client(client_socket, client_address):
         globals.players.append(client_socket)
         while True:
             data = client_socket.recv(1024)
+            if not data: continue
             try:
                 packet = json.loads(data.decode())
                 if packet["type"] == "login":
                     login_player(client_socket)
+                elif packet["type"] =="numplayers":
+                    num_players(client_socket)
+                elif packet ["type"] == "tictactoe_start":
+                    load_tic(client_socket)
+                elif packet ["type"] == "blackjack_start":
+                    load_black(client_socket)
                 else:
                     error_packet(client_socket)
             except json.JSONDecodeError:
-                print("Received non-JSON:", data.decode().strip())
-
-    except ConnectionResetError:
-        globals.players.remove(client_socket)
+                break
+    except ConnectionAbortedError:
         print(f"Client disconnected: {client_address[0]}:{client_address[1]}")
     finally:
         globals.players.remove(client_socket)
